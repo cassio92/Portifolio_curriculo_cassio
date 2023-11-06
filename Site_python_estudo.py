53e47ce92096f3d81f02b6e2e0d743ecc5e76947
Cassio de oliveira <57690531+cassio92@users.noreply.github.com>
2023-11-06 16:15:49 -0300

    Update Site_python_estudo.py

--- Site_python_estudo.py
+++ Site_python_estudo.py
@@ -1,18 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #criar a primeira pagina
 #route -> Estudos.com/
 #função -> O que você quer exibir naquela página
+#template
 @app.route("/")
 def homepage():
-    return "Esse é o meu primeiro site. Cassio vc vai ser progranmador python."
+    return render_template("homepage.html")
 
 @app.route("/contatos")
 def contatos():
-    return "<p>Nosso contatos são:</p> <p>E-mail: pythonimpressionador@gmail.com</p> e <p>Telefone: (31)99999999</p>"
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
 
 # colocar no ar
 if __name__ == "__main__":
     app.run(debug=True)
+
+# subir o site no servidor heroku
